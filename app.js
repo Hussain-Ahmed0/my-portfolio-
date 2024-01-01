70c2036beee2c4804eaff413a37da14e9c7198a1
Hussain Ahmed <155343946+Hussain-Ahmed0@users.noreply.github.com>
2024-01-01 12:40:18 -0800

    my portfolio
    How to Create Personal portfolio

---
+++ app.js
@@ -0,0 +1,9 @@
+
+var typed = new Typed(".text",{
+ 
+    strings: ["Fronted Developer." , "Backend Developer." , "web Developer."],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+}) ;
